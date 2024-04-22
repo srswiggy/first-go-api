0f5d804e1578248c1641b8dac020824fd818eb8f
Sarthak Rohatgi <sarthak.rohatgi_ftc@external.swiggy.in>
2024-04-22 23:07:07 +0530

    basic http server

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my API\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
